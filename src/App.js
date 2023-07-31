a638c02bf93fa0f48c98f2b0335c0d7508ea958c
lebattabdallahi <lebattabdallahi59@gmail.com>
2023-07-31 20:28:49 +0200

    fist commtto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import Register from "./Pages/Auth/Register";
 import Login from "./Pages/Auth/Login";
-
-import syle from "./components/styles/syle.css"
-//import Navbar from "./components/Sidebar";
-
-
-
 import DemandeConge from "./components/DemandeConge";
 import Historique from "./components/Historique";
 // import EmployeeStatus from "./components/Status";
-import { Routes,Route } from "react-router-dom";
-// import Sidebar from "./components/Sidebar";
-//import DemandeConge from "./components/DemandeConge";
-
-//import Sidebar from "./components/Sidbar";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-// import Nav from "./components/Nav";
-// import { useState } from "react";
- import Sidebar from "./components/Sidebar"
+
+
+import Sidebar from "./components/Sidebar"
 import { useState } from "react";
 import CongeList from "./components/Listes_demades";
 import AdminPage from "./components/AdminPage";
@@ -25,45 +15,40 @@ import Liste_des_demande from "./components/Listes_demades";
 import ValidationDemande from "./components/ValidationDemande";
 import RegisterDemande from "./components/RegeterDemande";
 
-
-//import Sidebar from "cdbreact/dist/components/Sidebar";
-
-
-
 function App() {
   const [media, setMedia] = useState(true)
   console.log("-------------------------------------")
   console.log(media)
   return (
-    
+
     <div  >
 
-      <div className="col ">
-        <Sidebar media={media} setMedia={setMedia}/>
+      <div >
+        <Sidebar media={media} setMedia={setMedia} />
       </div>
-          <Routes>
-        <Route path =""  element={<Home media={media}/>} /> 
-        {/* <Route path="/" element={<Sidebar/>} /> */}
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/demande" element={<DemandeConge/>} />
-        <Route path="/historique" element={<Historique/>} />
-        <Route path="y"element={<AdminPage/>} />
-        <Route path="/liste_des_demande" element={<Liste_des_demande/>} />
+      <Routes>
+        <Route path="" element={<Home media={media} />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/demande" element={<DemandeConge />} />
+        <Route path="/historique" element={<Historique />} />
+        <Route path="y" element={<AdminPage />} />
+        <Route path="/liste_des_demande" element={<Liste_des_demande />} />
         <Route path="/validationDemande/:id" element={<ValidationDemande />} />
         <Route path="/regeterDemande/:id" element={<RegisterDemande />} />
         {/* <Route path="/status" element={<EmployeeStatus/>} /> */}
         {/* <Route path="/navbar" element={<Navbar/>}/>
         <Route path="/demandeconge" element={<DemandeConge/>}/> */}
         {/* <Route path="/nav" element={<Nav/>}/> */}
-        
-         {/* <Route path="/sidbar" element={<Sidebar/>}/> */}
-         
+
+        {/* <Route path="/sidbar" element={<Sidebar/>}/> */}
+
       </Routes>
-        
-        </div>
-       
-        
+
+    </div>
+
+
   );
 }
 
